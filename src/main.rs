33b0e669c7c1ed43d76dfa06f89c9b46d9bb0af7
Allyson Bowles <abowles@hireology.com>
2018-10-01 23:38:53 -0500

    Valid C-ECHO-RQ created

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+extern crate dicom_core;
+
+mod c_echo;
+mod association;
+
+fn main() {
+    //let filename = String::from("test.dcm");
+    //print_file(&filename);
+
+    //println!("{:?}", c_echo::build_rq());
+
+    /*
+     * listen on some port
+     * accept associations
+     * do stuff with the associations, like:
+     * - respond to echo
+     * - store a study
+     * - perform a query
+     *
+     */
+}
